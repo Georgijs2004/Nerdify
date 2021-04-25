194f77e2a5a8a12bb3110e1ed7ecd063072a1977
Georgijs2004 <72558871+Georgijs2004@users.noreply.github.com>
2021-04-25 11:34:24 +0300

    anchor link
    anchor links

---
+++ main.js
@@ -0,0 +1,15 @@
+const anchors = document.querySelectorAll('.anchors_nav-bar a[href*="#"]');
+const yOffset = -0;
+
+for (anchor of anchors) {
+    if (anchor) {
+      anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        anchorId = this.getAttribute('data-redir');
+        scrollToElemenet =  document.querySelector(anchorId).getAttribute('Id')
+        const rect = document.getElementById(scrollToElemenet).getBoundingClientRect();
+        const y = rect.top + window.pageYOffset + yOffset;
+        window.scrollTo({top: y, behavior: 'smooth'});
+     })
+   }
+}
